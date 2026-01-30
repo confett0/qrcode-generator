2d974647e41e30a212a66f9a8e013c0993273940
confett0 <118047961+confett0@users.noreply.github.com>
2026-01-30 18:08:29 +0100

    Fix key property in QRCodeCard

--- src/components/QRgrid.jsx
+++ src/components/QRgrid.jsx
@@ -2,7 +2,7 @@ import QRCodeCard from './QRCodeCard';
 
 export default function QRgrid({ codeArray }) {
 
-    const qrCodeElements = codeArray.map(code => <QRCodeCard key={code} code={code} />)
+    const qrCodeElements = codeArray.map((code, index) => <QRCodeCard key={`${code}-${index}`} code={code} />)
 
     return <div className="qr-grid">
         {qrCodeElements}
