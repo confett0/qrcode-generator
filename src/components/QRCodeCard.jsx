455b76de2e5c0e90a680de62b7b5b51f82fb4c61
confett0 <118047961+confett0@users.noreply.github.com>
2026-01-30 18:11:06 +0100

    Fix sizing for QR codes SVG

--- src/components/QRCodeCard.jsx
+++ src/components/QRCodeCard.jsx
@@ -3,7 +3,7 @@ import { QRCodeSVG } from 'qrcode.react';
 export default function QRCodeCard({ code }) {
     return (
         <div className='qr-code-card'>
-            <QRCodeSVG value={code} level="Q" width={80} height={80} />
+            <QRCodeSVG value={code} level="Q" width="100%" height="100%" />
             <p>{code}</p>
         </div>
     )
