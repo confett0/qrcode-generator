9bce194e17c74aa08accc9654ba6b807a9fa83fe
confett0 <118047961+confett0@users.noreply.github.com>
2026-02-03 17:49:59 +0100

    Display correct number of column in preview + break longer text in cards

--- src/components/QRgrid.jsx
+++ src/components/QRgrid.jsx
@@ -2,9 +2,12 @@ import QRCodeCard from './QRCodeCard';
 
 export default function QRgrid({ codeArray, qrSize }) {
 
+    const printWidth = 210 // mm
+    const printMargin = 10
+    const columnNum = Math.floor((printWidth - printMargin * 2) / qrSize) // calculate number of columns for preview
     const qrCodeElements = codeArray.map((code, index) => <QRCodeCard key={`${code}-${index}`} code={code} />)
 
-    return <div className="qr-grid" style={{ '--qr-size': `${qrSize}mm` }}>
+    return <div className="qr-grid" style={{ '--qr-size': `${qrSize}mm`, '--column-num': `${columnNum}` }}>
         {qrCodeElements}
     </div>
 }
