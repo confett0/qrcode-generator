74d46c787ff910d681d0f3c35468ec2e08eb3658
confett0 <118047961+confett0@users.noreply.github.com>
2026-02-18 17:44:35 +0100

    Change print layout for bigger qr codes

--- src/components/QRgrid.jsx
+++ src/components/QRgrid.jsx
@@ -4,7 +4,9 @@ export default function QRgrid({ codeArray, qrSize }) {
   const printWidth = 210; // mm
   const printMargin = 10;
   // calculate number of columns for screen preview
-  const columnNum = Math.floor((printWidth - printMargin * 2) / qrSize);
+  const columnNum =
+    qrSize === 40 ? 3 : Math.floor((printWidth - printMargin * 2) / qrSize);
+  const gap = qrSize === 40 ? "1.5rem 3rem" : 0; // 35mm qr codes require a different print layout
   const qrCodeElements = codeArray.map((code, index) => (
     <QRCodeCard key={`${code}-${index}`} code={code} />
   ));
@@ -12,7 +14,11 @@ export default function QRgrid({ codeArray, qrSize }) {
   return (
     <div
       className="qr-grid"
-      style={{ "--qr-size": `${qrSize}mm`, "--column-num": `${columnNum}` }}
+      style={{
+        "--qr-size": `${qrSize}mm`,
+        "--column-num": columnNum,
+        gap: gap,
+      }}
     >
       {qrCodeElements}
     </div>
